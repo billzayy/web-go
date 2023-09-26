b3e3a34107d72ace1bf2f85e2bf5854edd083877
billzayy <coderbillzay@gmail.com>
2023-09-26 16:52:55 +0700

    fix folder

--- server/main.go
+++ server/main.go
@@ -1,1 +1,11 @@
 package main
+
+// "github.com/billzayy/web-go/server/models"
+// "github.com/billzayy/web-go/server/middlewares"
+// "github.com/billzayy/web-go/server/routes"
+// "github.com/billzayy/web-go/server/utils"
+// "github.com/billzayy/web-go/server/auth"
+
+func main() {
+
+}
